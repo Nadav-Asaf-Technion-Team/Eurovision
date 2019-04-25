9a9cf858da3a0f5adb60a5fb3a33a717959e48cf
Asaf Bialystok <asaf.b@campus.technion.ac.il>
2019-04-26 00:49:57 +0300

    cleaned main.c

--- main.c
+++ main.c
@@ -4,14 +4,5 @@
 //testing-branch before you work on it, and make sure you go
 //back to master when you're done.
 int main() {
-	Eurovision eurovision = eurovisionCreate();
-	int results[10] = { 101, 102, 103, 104, 105 };
-	if (eurovisionAddJudge(eurovision, 55, "john", results) == EUROVISION_STATE_NOT_EXIST) {
-		printf("an error has occured\n");
-		return 1;
-	}
-	else printf("number of judges is %d\n", getAmountOfJudges(eurovision));
-	eurovisionRemoveJudge(eurovision, 55);
-	printf("number of judges is %d", getAmountOfJudges(eurovision));
 	return 0;
 }
